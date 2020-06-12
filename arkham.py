981f8e5d9bc9af1964a50953ec619cac4902e43a
Alex Harding <alexharding@rti.org>
2020-06-12 16:04:18 -0400

    refactoring to single file - bot doesn't like being split up

--- arkham.py
+++ arkham.py
@@ -1,7 +1,9 @@
 import os
+import random
+import re
+import requests
+
 import discord
-from arkhamdb import *
-from dice import *
 from discord.ext import commands
 
 from dotenv import load_dotenv
@@ -9,9 +11,189 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
+
 bot = commands.Bot(command_prefix='!')
 # gets all cards including encounters
 cards = []
+
+## Commands
+def get_cards():
+    global cards
+    cards = requests.get(
+        'https://www.arkhamdb.com/api/public/cards?encounter=1').json()
+
+# creates embedded link with card image
+def embed_card(card, image=True):
+    e = discord.Embed()
+    if image and card.get('imagesrc', None):
+        image_url = "https://www.arkhamdb.com" + card.get('imagesrc')
+        e.set_image(url=image_url)
+    e.url = card.get('url', None)
+    e.title = card.get('name', None)
+    if card.get('xp', 0):
+        e.title += " (" + str(card.get('xp', '-')) + ")"
+    if card.get('subname', None):
+        e.description = card.get('subname')
+    return e
+
+
+@bot.command(name='refresh')
+async def refresh_cards(ctx):
+    get_cards()
+    await ctx.send('Card pool refreshed')
+
+@bot.command(name='upgrade')
+async def get_random_upgraded_card(ctx, target):
+    targets = list(filter(lambda card: card.get('name', "").lower() == target.lower() and card.get('xp', 0) is not 0, cards))
+    if len(targets):
+        e = embed_card(random.choice(list(targets)))
+        await ctx.send(embed=e)
+    else:
+        await ctx.send("No matches for " + target)
+
+@bot.command(name='weakness')
+async def get_random_basic_weakness(ctx, *args):
+    weaknesses = list(filter(lambda card: card.get('subtype_code', None) ==
+                             'basicweakness' and card.get('name', "") is not "Random Basic Weakness", cards))
+    if len(list(args)):
+        def matchTraits(card):
+            matches = 0
+            for trait in list(args):
+                if trait.lower() in card.get('traits', '').lower():
+                    matches += 1
+            if matches > 0:
+                return True
+            return False
+
+        weaknesses = filter(matchTraits, weaknesses)
+    weakness = random.choice(list(weaknesses))
+    e = embed_card(weakness)
+    await ctx.send(embed=e)
+
+@bot.command(name='card')
+async def search_card(ctx, *args):
+    if(len(list(args)) == 0):
+        name = 'ancient evils'
+    else:
+        name = ' '.join(list(args))
+    matches = list(filter(lambda card: name.lower() in card.get('name', '').lower(), cards))
+    if len(matches) and len(matches) <= 3:
+        for match in matches:
+            e = embed_card(match)
+            await ctx.send(embed=e)
+    elif len(matches) > 3 and len(matches) <= 10:
+        for match in matches:
+            e = embed_card(match, False)
+            await ctx.send(embed=e)
+    elif len(matches) > 10:
+        await ctx.send('More than 10 matches, please refine your search')
+    else:
+        await ctx.send('No matches')
+
+@bot.command(name='investigator')
+async def random_investigator(ctx, faction=None):
+    investigators = list(filter(lambda card: card.get('type_code', '') == 'investigator' and card.get('permanent', True) and card.get('hidden', True), cards))
+    if faction:
+        investigators = list(filter(lambda card: card.get('faction_code', '') == faction.lower(), investigators))
+    if len(investigators) > 0:
+        choice = random.choice(list(investigators))
+        e = embed_card(choice)
+        await ctx.send(embed=e)
+    else:
+        await ctx.send('No matches found - invalid faction')
+
+
+## Dice commands
+@bot.command(name='cointoss')
+async def cointoss(ctx):
+    await ctx.send(random.choice(['Heads', 'Tails']))
+
+@bot.command(name='roll')
+async def roll_dice(ctx, dice):
+    split = dice.lower().split('d')
+    if split[0] == '':
+        split[0] = 1
+    try:
+        q = int(split[0])
+        d = int(split[1])
+    except:
+        await ctx.send("Invalid die type or quantity")
+        return
+
+    if q > 100 or d > 100:
+        await ctx.send("I can only count to 100 on either hand. Try again with 100 or less dice/sides.")
+    else:
+        rolls = []
+        for i in range(q):
+            rolls.append(random.randrange(1, d+1))
+        e = discord.Embed()
+        e.title= 'Roll ' + dice
+        e.description = ''
+        if len(rolls) > 2:
+            e.description += 'High Roll: ' + str(max(rolls)) + '\n'
+            e.description += 'Low Roll: ' + str(min(rolls)) + '\n'
+        e.description += 'Total: ' + str(sum(rolls)) + '\n'
+        e.description += '\n Rolls:'
+        for roll in rolls:
+            e.description += '\n' + str(roll)
+
+        await ctx.send(embed=e)
+
+@bot.command(name="pickone")
+async def pickone(ctx, *args):
+    await ctx.send(random.choice(args))
+
+async def embed_deck(message):
+    content = message.content.lower()
+    # parse message to get deck id and type (decklist or deck)
+    deckId = None
+    deckType = None
+    if "arkhamdb.com/deck/view/" in content:
+        deckId = re.search('(?<=arkhamdb.com/deck/view/)(.{6})', content)
+        deckType = 'deck'
+    if "https://arkhamdb.com/decklist/" in content:
+        deckId = re.search('(?<=arkhamdb.com/decklist/view/)(.{5})', content)
+        deckType = 'decklist'
+
+    if deckId:
+        # get deck from arkhamdb api
+        deckId = deckId.group()
+        if deckType == 'deck':
+            apiString = 'https://arkhamdb.com/api/public/deck/' + deckId
+        if deckType == 'decklist':
+            apiString = 'https://arkhamdb.com/api/public/decklist/' + deckId
+        deckJson = requests.get(apiString).json()
+        # create initial embed using investigator card image
+        gator = list(filter(lambda card: card.get('code', 0) == deckJson.get('investigator_code', None), cards))[0]
+        e = discord.Embed()
+        e.description = ''
+        # set title to deck name with appropriate url
+        e.title = deckJson.get('name', "") + " " + deckJson.get('version', "")
+        if deckType == 'deck':
+            e.url = 'https://arkhamdb.com/deck/view/' + deckId
+        if (deckType == 'decklist'):
+            e.url = 'https://arkhamdb.com/decklist/view/'+ deckId
+        categories = ['Asset', 'Permanent', 'Event', 'Skill', 'Treachery']
+        deckCards = list(filter(lambda card: card.get('code', '') in deckJson.get('slots', {}).keys(), cards))
+        for category in categories:
+            if(category == 'Permanent'):
+                categoryCards = list(filter(lambda card: card.get('permanent', False) == True, deckCards))
+            else:
+                categoryCards = list(filter(lambda card: card.get('type_code', '') == category.lower() and card.get('permanent', False) == False, deckCards))
+            if(category == 'Treachery'):
+                e.description += '\n' + 'Treacheries:'
+            else:
+                e.description += '\n ' + category + 's:'
+            for card in categoryCards:
+                cardString = str(deckJson.get('slots')[card.get('code')]) + 'x '
+                cardString += card.get('name', '')
+                if card.get('xp', 0) > 0:
+                    cardString += ' (' + str(card.get('xp', 0)) + ')'
+                e.description += '\n' + cardString
+            e.description += '\n'
+        await message.channel.send(embed=e)
+    else:
+        await message.channel.send('Deck URL detected, unable to extract deck ID')
 
 @bot.event
 async def on_message(message):
@@ -27,5 +209,4 @@ async def on_message(message):
     await bot.process_commands(message)
 
 get_cards()
-print(token)
 bot.run(token)
