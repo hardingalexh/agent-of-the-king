ef1c7fb483169c7746f59b402f9aecee1cf492d9
Alex Harding <5278388+hardingalexh@users.noreply.github.com>
2020-07-25 12:08:18 -0400

    adding player count dependent variables, timer

--- cogs/blob.py
+++ cogs/blob.py
@@ -1,4 +1,6 @@
 import discord
+import math
+from datetime import datetime
 from discord.ext import commands
 
 class Blob(commands.Cog):
@@ -8,7 +10,8 @@ class Blob(commands.Cog):
         self.players = 0
         self.clues = 0
         self.damage = 0
-        self.supplies = 0
+        self.countermeasures = 0
+        self.starttime = 0
     
     @commands.group(brief="Manages state for playing the blob that ate everything, using multiple commands. Type !help blob for more info.")
     async def blob(self, ctx):
@@ -17,26 +20,32 @@ class Blob(commands.Cog):
 
     @blob.command(usage="<player count>", help="Sets up a new blob scenario for the given player count")
     async def setup(self, ctx, param):
-        self.players = param
+        self.players = int(param)
         self.clues = 0
         self.damage = 0
-        self.supplies = 0
+        self.countermeasures = math.ceil(int(param) / 2)
+        self.starttime = datetime.now()
         await ctx.send('Set up the blob for ' + str(param) + ' players')
         await self.status(ctx)
 
     @blob.command(help="Prints the status of the current blob event")    
     async def status(self, ctx):
         e = discord.Embed()
+        endtime = self.starttime + datetime.timedelta(seconds=60*180)
+        timeremaining = endtime - datetime.now()
         e.title = "The Blob That Ate Everything"
         e.description = ""
-        e.description += "\n Supplies: " + str(self.supplies)
-        e.description += "\n Clues: " + str(self.clues)
-        e.description += "\n Damage: " + str(self.damage)
+        e.description += "\n Start Time: " + self.starttime.strptime("%H:%M")
+        e.description += "\n End Time: " + self.endtime.strptime("%H:%M")
+        e.description += "\n Time Remaining: " + str(math.floor(timeremaining.seconds / 60)) + ' Minutes'
+        e.description += "\n Countermeasures: " + str(self.countermeasures)
+        e.description += "\n Clues: " + str(self.clues) = '/' + str(self.players * 2)
+        e.description += "\n Damage: " + str(self.damage) + '/' + str(self.players * 15)
         await ctx.send(embed=e)
 
-    @blob.command(usage="<quantity>", help="Adds or subtracts the given number of supplies. For example, !blob supplies 1 adds 1 supply, !blob supplies -3 removes 3 supplies.")
-    async def supplies(self, ctx, quantity):
-        await self._add(ctx, 'supplies', quantity)
+    @blob.command(usage="<quantity>", help="Adds or subtracts the given number of countermeasures. For example, !blob countermeasures 1 adds 1 supply, !blob countermeasures -3 removes 3 countermeasures.")
+    async def countermeasures(self, ctx, quantity):
+        await self._add(ctx, 'countermeasures', quantity)
     
     @blob.command(usage="<quantity>", help="Adds or subtracts the given number of damage. For example, !blob damage 1 adds 1 supply, !blob damage -3 removes 3 damage.")
     async def damage(self, ctx, quantity):
@@ -48,8 +57,8 @@ class Blob(commands.Cog):
     
     async def _add(self, ctx, category, quantity):
         category = category.lower()
-        if category not in ['supplies', 'clues', 'damage']:
-            await ctx.send(category + ' is not a valid category. Try again with supplies, damage or clues.')    
+        if category not in ['countermeasures', 'clues', 'damage']:
+            await ctx.send(category + ' is not a valid category. Try again with countermeasures, damage or clues.')    
             return
         setattr(self, category, getattr(self, category) + int(quantity))
         await ctx.send(str(quantity) + ' ' + category)
