8f9ac937b7fc71c867758f9055e3248470df0589
Alex Harding <alexharding@rti.org>
2020-08-05 13:20:20 -0400

    adding multiple special dice

--- cogs/funko.py
+++ cogs/funko.py
@@ -7,25 +7,38 @@ class Funko(commands.Cog):
         self.bot = bot
         self._last_member = None
     
-    @commands.command(usage="<quantity> <character (optional)", help="Rolls dice for the funkoverse strategy game. Specify a character for character-specific die rolls.")
-    async def funko(self, ctx, quantity, character=None):
+    @commands.command(usage="<quantity> <any number of special dice>", help="Rolls dice for the funkoverse strategy game. Specify a character for character-specific die rolls.")
+    async def funko(self, ctx, *args):
+        args = list(args)
         shield = u'\U0001F6E1'
         boom = u"\U0001F4A5"
         ex =  "!!!"
         faces = [shield, shield, boom, boom, boom, ex]
-        jeffFaces = ['Blank', 'Blank', 'Blank', ex, ex, ex]
+        special = {
+            'chaos': ['Blank', 'Blank', 'Blank', ex, ex, ex],
+            'champion': [shield, shield, shield, ex, ex, ex]
+        }
         e = discord.Embed()
         e.title = "Funkoverse Strategy Game Dice Roll"
         e.description = ''
-        quantity = int(quantity)
-
+        try:
+            quantity = int(args[0])
+        except:
+            await ctx.send('First argument must be a valid number')
+        
         if quantity > 100:
             await ctx.send("I can only count to 100 on either hand. Try again with 100 or less dice.")
             return
 
-        for roll in range(int(quantity)):
-            if roll == 0 and character and character.lower() == 'jeff':
-                e.description += '\n' + random.choice(jeffFaces)
-            else:
+        for roll in range(quantity):
                 e.description += '\n' + random.choice(faces)
+
+        if len(args) > 1:
+            args.pop(0)
+            for arg in args:
+                if arg.lower() in special.keys():
+                    e.description += '\n \n ' + arg + ' Die:'
+                    e.description += '\n' + random.choice(special.get(arg.lower()))
+                else:
+                    await ctx.send(arg + 'is not a valid special die.')
         await ctx.send(embed=e)
