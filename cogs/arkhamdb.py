ab8fb2845f343c9ad46204e81ea937f71c0cb16e
Alex Harding <5278388+hardingalexh@users.noreply.github.com>
2020-10-05 10:31:31 -0400

    reordering if block to avoid errors

--- cogs/arkhamdb.py
+++ cogs/arkhamdb.py
@@ -162,10 +162,10 @@ class Arkhamdb(commands.Cog):
             endpos = levelSearch.span()[0] - 1
             searchTerm = arg[0:endpos].strip().lower()
             levelTerm = levelSearch.group()
-            if int(levelTerm):
-                matches = list(filter(lambda card: (searchTerm.lower() in card.get('name', '').lower()) and (card.get('xp', 0) == int(levelTerm)), self.cards))
-            elif levelTerm.lower() == 'u':
+            if levelTerm.lower() == 'u':
                 matches = list(filter(lambda card: (searchTerm.lower() in card.get('name', '').lower()) and (card.get('xp', 0) > 0), self.cards))
+            elif int(levelTerm):
+                matches = list(filter(lambda card: (searchTerm.lower() in card.get('name', '').lower()) and (card.get('xp', 0) == int(levelTerm)), self.cards))
             else:
                 await ctx.send('Invalid input for level')
         else:
